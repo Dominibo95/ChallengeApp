16f1687ede0258a6cf3b59d1431348a5b419a8db
Dominibo95 <dominibo95@gmail.com>
2023-11-09 22:15:25 +0000

    day 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿var netCoreVer = System.Environment.Version;
-Console.WriteLine("Hello Dominik! on dotnet: " + netCoreVer);
+﻿
+var name = "Dominik";
+int age = 16;
+bool isWoman = true;
+
+if (age == 33 && name == "Ewa" && isWoman == true)
+{
+    Console.WriteLine("Ewa , lat 33");
+}
+        else if (age < 18 && isWoman == false)
+        {
+            Console.WriteLine("Niepełnoletni Mezczyzna");
+        }
+        else if (age < 30 && isWoman == true)
+        {
+        Console.WriteLine("Kobieta ponizej 30 lat");
+        }
+else
+{
+    Console.WriteLine("Dane nie pasują");
+}
