e0c655aecb4ac9dff3416f7b9600bcf6ff51ed31
Dominibo95 <dominibo95@gmail.com>
2023-11-10 19:02:49 +0000

    fix code formatting

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection.Metadata;
+
 var name = "Dominik";
 int age = 16;
 bool isWoman = true;
@@ -7,14 +9,14 @@ if (age == 33 && name == "Ewa" && isWoman == true)
 {
     Console.WriteLine("Ewa , lat 33");
 }
-        else if (age < 18 && isWoman == false)
-        {
-            Console.WriteLine("Niepełnoletni Mezczyzna");
-        }
-        else if (age < 30 && isWoman == true)
-        {
-        Console.WriteLine("Kobieta ponizej 30 lat");
-        }
+else if (age < 18 && isWoman == false)
+{
+    Console.WriteLine("Niepełnoletni Mezczyzna");
+}
+else if (age < 30 && isWoman == true)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
 else
 {
     Console.WriteLine("Dane nie pasują");
