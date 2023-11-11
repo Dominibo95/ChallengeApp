fd240ecc7430e9fc2df3ed7185d2caee4d039155
Dominibo95 <dominibo95@gmail.com>
2023-11-11 01:40:25 +0000

    day 5 trainig for and foreach

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,20 +1,23 @@
-﻿var name = "Dominik";
-int age = 16;
-bool isWoman = true;
+﻿int number = 123455678;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+int[] counter = new int[10];
 
-if (age == 33 && name == "Ewa" && isWoman == true)
+foreach (char letter in letters)
 {
-    Console.WriteLine("Ewa , lat 33");
+    if (letter == '0') { counter[0]++; }
+    else if (letter == '1') { counter[1]++; }
+    else if (letter == '2') { counter[2]++; }
+    else if (letter == '3') { counter[3]++; }
+    else if (letter == '4') { counter[4]++; }
+    else if (letter == '5') { counter[5]++; }
+    else if (letter == '6') { counter[6]++; }
+    else if (letter == '7') { counter[7]++; }
+    else if (letter == '8') { counter[8]++; }
+    else if (letter == '9') { counter[9]++; }
 }
-else if (age < 18 && isWoman == false)
+
+for (int i = 0; i < counter.Length; i++)
 {
-    Console.WriteLine("Niepełnoletni Mezczyzna");
-}
-else if (age < 30 && isWoman == true)
-{
-    Console.WriteLine("Kobieta ponizej 30 lat");
-}
-else
-{
-    Console.WriteLine("Dane nie pasują");
+    Console.WriteLine(i + " =>" + counter[i]);
 }
