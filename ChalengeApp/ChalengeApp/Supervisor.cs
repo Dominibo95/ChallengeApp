2242170c1e8534160d1f974a271b44dda27e2046
Dominibo95 <dominibo95@gmail.com>
2023-11-14 22:39:39 +0000

    day 15 : create interface and supervisor class

--- ChalengeApp/ChalengeApp/Supervisor.cs
+++ ChalengeApp/ChalengeApp/Supervisor.cs
@@ -1,22 +1,21 @@
-﻿namespace ChallengeApp
+﻿
+
+namespace ChallengeApp
 {
-    public class Employee : IEmployee
+    public class Supervisor : IEmployee
     {
         private List<float> grades = new List<float>();
-
-
-        public Employee(string name, string surname, int age)
+        public Supervisor(string name, string surname, char gender, int age)
         {
             this.Name = name;
             this.Surname = surname;
+            this.Gender = gender;
             this.Age = age;
         }
         public string Name { get; private set; }
         public string Surname { get; private set; }
-
+        public char Gender { get; private set; }
         public int Age { get; private set; }
-
-
 
         public void AddGrade(float grade)
         {
@@ -30,11 +29,45 @@
                 throw new Exception("invalid grade value");
             }
         }
+
         public void AddGrade(short grade)
         {
-            var value = (float)grade;
-            this.AddGrade(value);
+            throw new NotImplementedException();
         }
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float value))
+            {
+                this.AddGrade(value);
+            }
+            else if (char.TryParse(grade, out char gradeIsChar))
+            {
+                this.AddGrade(gradeIsChar);
+            }
+            else
+            {
+                throw new Exception("String is not float or char");
+            }
+        }
+
+        public void AddGrade(long grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(double grade)
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
+        public void AddGrade(int grade)
+        {
+            float gradeAsFloat = grade;
+            this.AddGrade(gradeAsFloat);
+        }
+
         public void AddGrade(char grade)
         {
             switch (grade)
@@ -61,37 +94,6 @@
                     break;
                 default:
                     throw new Exception("Wrong Letter");
-            }
-        }
-        public void AddGrade(long grade)
-        {
-            var value = (float)grade;
-            this.AddGrade(value);
-        }
-        public void AddGrade(int grade)
-        {
-            float gradeAsFloat = grade;
-            this.AddGrade(gradeAsFloat);
-        }
-
-        public void AddGrade(double grade)
-        {
-            var value = (int)grade;
-            this.AddGrade(value);
-        }
-        public void AddGrade(string grade)
-        {
-            if (float.TryParse(grade, out float value))
-            {
-                this.AddGrade(value);
-            }
-            else if (char.TryParse(grade, out char gradeIsChar))
-            {
-                this.AddGrade(gradeIsChar);
-            }
-            else
-            {
-                throw new Exception("String is not float or char");
             }
         }
 
