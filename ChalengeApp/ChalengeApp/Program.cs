15ca011b4ee54f4e6e97a335e0d38404b705410c
Dominibo95 <dominibo95@gmail.com>
2023-11-14 11:49:41 +0000

    Prepare for day12

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -10,32 +10,9 @@ employee.AddGrade(3);
 employee.AddGrade(6);
 
 var statistics = employee.GetStatistics();
-var statistics1 = employee.GetStatisticsWithForEach();
-var statistics2 = employee.GetStatisticsWithFor();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-var statistics4 = employee.GetStatisticsWithWhile();
+
 
 Console.WriteLine("Wyniki dla Metody GetStatistics: ");
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
-
-Console.WriteLine("Wyniki dla Metody GetStatisticsWithForEach: ");
-Console.WriteLine($"Average: {statistics1.Average:N2}");
-Console.WriteLine($"Min: {statistics1.Min}");
-Console.WriteLine($"Max: {statistics1.Max}");
-
-Console.WriteLine("Wyniki dla Metody GetStatisticsWithFor: ");
-Console.WriteLine($"Average: {statistics2.Average:N2}");
-Console.WriteLine($"Min: {statistics2.Min}");
-Console.WriteLine($"Max: {statistics2.Max}");
-
-Console.WriteLine("Wyniki dla Metody GetStatisticsWithDoWhile: ");
-Console.WriteLine($"Average: {statistics3.Average:N2}");
-Console.WriteLine($"Min: {statistics3.Min}");
-Console.WriteLine($"Max: {statistics3.Max}");
-
-Console.WriteLine("Wyniki dla Metody GetStatisticsWithWhile:");
-Console.WriteLine($"Average: {statistics4.Average:N2}");
-Console.WriteLine($"Min: {statistics4.Min}");
-Console.WriteLine($"Max: {statistics4.Max}");
